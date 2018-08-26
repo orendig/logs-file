3c2dc604e4a87bfd2ccc594248a76a5fc4516e12
Oren Digmi <orendig@gmail.com>
2018-08-26 22:18:31 +0300

    2: implements fetchLogs func using /tmp/alilog.txt

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 
+	"io"
 	"log"
+	"math"
 	"os"
 )
 
@@ -20,4 +22,64 @@ func main() {
 		fmt.Fprintf(file, "Hi, this is line number %v\n", i)
 	}
 
+	// Task2: call fetchLogs
+	fetchLogs("/tmp/alilog.txt", 96, 13, true)
+}
+
+func fetchLogs(logFilePath string, numberOfLines, offset int64, reverse bool) {
+	// Calculate the offset position and the fetch size
+	byteToRead, byteOffset := calcByteToReadAndOffset(numberOfLines, offset, reverse)
+	file, err := os.Open(logFilePath)
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	defer file.Close()
+
+	byteBuffer := make([]byte, byteToRead)
+
+	// Reads len(byteBuffer) bytes from the File starting at byteOffset.
+	fetchBytes, err := file.ReadAt(byteBuffer, byteOffset)
+
+	if err != nil {
+		if err != io.EOF {
+			log.Fatal("Error while trying to fetchLogs", err.Error())
+			return
+		}
+	}
+
+	fmt.Println(string(byteBuffer[:fetchBytes]))
+}
+
+func calcByteToReadAndOffset(numberOfLines, offset int64, reverse bool) (int64, int64) {
+	// Check reverse flag
+	if reverse {
+		offset = 10000 - numberOfLines - offset
+	}
+
+	// Calculate totalLines: sum of numberOfLines and offset
+	totalLines := numberOfLines + offset
+	byteOffset := offset * 25
+	byteToRead := totalLines * 25
+
+	// Calculate byteOffset and byteToRead by iterating the totalLines
+	// And add the delta bytes
+	// e.x. if offset = 13 and numberOfLines = 96
+	// byteOffset will be 342 (9*26 + 4*27)
+	// byteToRead will be 2602 (86*27 + 10*28)
+	for i := 0; totalLines > 0; i++ {
+		reduce := 9 * int64(math.Pow10(i))
+		byteOffset += offset
+		offset -= reduce
+
+		byteToRead += totalLines
+		totalLines -= reduce
+
+		if offset < 0 {
+			offset = 0
+		}
+	}
+
+	return byteToRead - byteOffset, byteOffset
 }
